5433d0c70f95df0c0bb7d29f38ba901bd2895f51
Virgile <virgile.bessiere@gmail.com>
2024-07-21 17:57:20 +0200

    fixed dockerfiles + cleaned .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,19 +7,16 @@ const passwordCollaborationRoutes = require("./routes/passwordCollaboration");
 const libraryRoutes = require("./routes/library");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const rateLimit = require("express-rate-limit");
 const limiter = require("./utils/expressratelimit");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-const PORTFRONTEND = process.env.PORT_FRONTEND;
-const IP1 = process.env.IP1;
-const IP2 = process.env.IP2;
+const IP = process.env.IP;
 
 const corsOptions = {
-  origin: IP2 || `${IP1}`,
+  origin: IP,
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -45,6 +42,10 @@ app.use("/libraries", libraryRoutes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Internal Server Error" });
+});
+
+app.get("/", (req, res) => {
+  res.send("hello wodwrdwdewld2")
 });
 
 app.listen(PORT, () => {
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.32:3000", // EDIT FAST
+  baseURL: `http://${process.env.IP}:${process.env.PORT}`,
 });
 
 // POST
